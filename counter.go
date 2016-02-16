422c4b36dfe7d1f4232eb531cee1fffd8c104ee5
cmc <cr.rey.clement@gmail.com>
2016-02-16 16:51:26 +0100

    improved NewCounter

--- counter.go
+++ counter.go
@@ -7,7 +7,7 @@ import "sync/atomic"
 // Counter implements an atomic counter with a threshold.
 //
 // Once it reaches its threshold, the Counter is decremented by the value of
-// said threshold, and a set of user-defined callbacks is executed.
+// said threshold, and the associated set of user-defined callbacks is executed.
 type Counter func() int64
 
 // Callback is executed once a Counter reaches its threshold.
@@ -17,15 +17,19 @@ type Callback func(int64)
 // `threshold` values.
 //
 // Once it reaches its threshold, the Counter is decremented by the value of
-// said threshold, and a set of user-defined callbacks is executed.
-// Each callback is executed in its own goroutine.
+// said threshold, and the associated set of user-defined callbacks is executed.
 func NewCounter(start, threshold int64, callbacks ...Callback) Counter {
+	if threshold < 1 {
+		panic("threshold < 1")
+	}
+
 	cnt := start
 	return func() int64 {
-		x := atomic.SwapInt64(&cnt, atomic.AddInt64(&cnt, 1))
-		if x == threshold {
+		x := atomic.AddInt64(&cnt, 1)
+		if x >= threshold && x%threshold == 0 {
+			atomic.AddInt64(&cnt, -threshold)
 			for _, cb := range callbacks {
-				go cb(x)
+				cb(threshold)
 			}
 		}
 		return x
