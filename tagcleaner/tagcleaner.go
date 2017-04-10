e530e6f59eed3d30ca518a12814084bd681465ea
Clement Rey <cr.rey.clement@gmail.com>
2017-04-10 12:00:08 +0200

    added tagcleaner + test

---
+++ tagcleaner/tagcleaner.go
@@ -0,0 +1,27 @@
+package tagcleaner
+
+import (
+	"reflect"
+)
+
+// -----------------------------------------------------------------------------
+
+// Clean recursively walks through `t` and annihilates any struct-tag it might
+// find along the way.
+func Clean(t reflect.Type) reflect.Type {
+	switch t.Kind() {
+	case reflect.Ptr, reflect.Map:
+		return Clean(t.Elem())
+	case reflect.Struct:
+		fields := make([]reflect.StructField, t.NumField())
+		for i := 0; i < t.NumField(); i++ {
+			f := t.Field(i)
+			f.Tag = ""
+			f.Type = Clean(f.Type)
+			fields[i] = f
+		}
+		return reflect.StructOf(fields)
+	default:
+		return t
+	}
+}
---
+++ tagcleaner/tagcleaner_test.go
@@ -0,0 +1,45 @@
+package tagcleaner
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+// -----------------------------------------------------------------------------
+
+func TestTagcleaner_Clean(t *testing.T) {
+	type someStruct struct {
+		A string   "tag-A"
+		B []string "tag-B"
+		C map[string]*struct {
+			A string "tag-C-A"
+			B string "tag-C-B"
+		} "tag-C"
+		D map[int32]string "tag-D"
+		E struct {
+			A int64 "tag-E-A"
+			B int32 "tag-E-B"
+		} "tag-E"
+		F *struct {
+			A *struct {
+				A int64 "tag-F-A-A"
+				B int32 "tag-F-A-B"
+			} "tag-F-A"
+		} "tag-F"
+		G struct {
+			A string "tag-G-A"
+			B string "tag-G-B"
+		} "tag-G"
+	}
+
+	raw := fmt.Sprintf("%s", reflect.TypeOf(someStruct{}))
+	if !strings.Contains(raw, "tag") {
+		t.Error("raw dump should contain tags")
+	}
+	cleansed := fmt.Sprintf("%s", Clean(reflect.TypeOf(someStruct{})))
+	if strings.Contains(cleansed, "tag") {
+		t.Error("cleansed dump should NOT contain tags")
+	}
+}
